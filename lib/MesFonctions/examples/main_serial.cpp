a37f4eadc58badf8de3cc9d4ced1d17770206b1e
Eric Benoit <eric.benoit@univ-smb.fr>
2022-04-14 19:15:34 +0200

    [misc] ajout d'un exemple

---
+++ lib/MesFonctions/examples/main_serial.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+#include <GEIIutil.h>
+#include <MySerial.h>
+
+
+void setup(){
+	InitPort();
+	InitLiaisonSerie();
+	InitLCD();				//ordre des inits important
+	lcd.print("TP4");
+	//Serial.println("TP4");
+	EcritCar('T');
+	EcritCar('P');
+	EcritCar('4');
+	EcritCar(13);
+	EcritCar(10);
+}
+
+//SANS FIN 
+
+void loop() {
+	// EcritCar('A');
+	// delay(2);
+
+	char l_char_C = LitCarNB();
+    if(l_char_C != 0){
+		EcritCar(':');
+		EcritCar(l_char_C);
+	}
+}
+
